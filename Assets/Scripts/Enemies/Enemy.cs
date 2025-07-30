62228a5e9e037a7fb8f27b34cdaadd322d2f6649
LupidCorvid <lucidcanine921@gmail.com>
2025-07-30 18:49:39 -0400

    Linear enemy behavior mostly done

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,32 +4,20 @@ namespace Enemies {
     public abstract class Enemy : MonoBehaviour {
         public Vector3 m_spawnLocation;
         public int m_enemyType;
-        public int m_damage;
-        public int m_health;
+        public int m_damage { get; set; }
+        public int m_health { get; set; }
+        public float m_speed { get; set; }
 
         public void DestroyEnemy()
         {
             //TODO: Play death animation
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
         public void LinearMovement(Vector3 target)
         {
-
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, target, m_speed);
         }
 
         public abstract void Behavior();
-    }
-
-    public class LinearEnemy : Enemy
-    {
-        public LinearEnemy(Vector3 spawnLocation)
-        {
-            m_spawnLocation = spawnLocation;
-        }
-
-        public override void Behavior()
-        {
-            print("WIP");
-        }
     }
 }
---
+++ Assets/Scripts/Enemies/LinearEnemy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Enemies
+{
+    public class LinearEnemy : Enemy
+    {
+        public void Start()
+        {
+            m_speed = .005f;
+            m_damage = 20;
+            m_health = 20;
+        }
+        public void Update()
+        {
+            Behavior();
+        }
+
+        public LinearEnemy(Vector3 spawnLocation, float speed, int damage, int health)
+        {
+            m_spawnLocation = spawnLocation;
+            m_speed = speed; m_damage = damage; m_health = health;
+        }
+
+        public override void Behavior()
+        {
+            print(m_speed);
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            LinearMovement(player.transform.position);
+        }
+    }
+}
