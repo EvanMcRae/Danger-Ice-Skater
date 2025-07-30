3dc5cac1ee00380fee3d5d36d80e1bf74c8f88ba
RelaxItsDax <relaxitsdax@gmail.com>
2025-07-30 17:12:23 -0400

    Changes to enemy mapping to make it scriptable object based + Documentation

--- Assets/Scripts/Waves/WaveSpawner.cs
+++ Assets/Scripts/Waves/WaveSpawner.cs
@@ -5,10 +5,21 @@ namespace Waves {
     public class WaveSpawner : MonoBehaviour {
         public WaveManager waveManagerSO;
 
+        /// <summary>
+        ///     The current wave index.
+        /// </summary>
         public int currentWave = -1;
+        
+        /// <summary>
+        ///     The current sub wave index.
+        /// </summary>
         public int currentSubWave = -1;
 
 
+        /// <summary>
+        ///     Progresses the spawner to the next wave. Does not do additional checks.
+        /// </summary>
+        /// <returns>True if the wave was properly progressed, else false.</returns>
         public bool NextWave() {
             if (waveManagerSO.waves.Count <= currentWave + 1) {
                 Debug.Log("All waves cleared!");
@@ -25,6 +36,10 @@ namespace Waves {
             return true;
         }
 
+        /// <summary>
+        ///     Progresses the spawner to the next sub wave. Does not do additional checks.
+        /// </summary>
+        /// <returns>True if the sub wave was properly progressed, else false.</returns>
         public bool NextSubWave() {
             if (waveManagerSO.waves.Count <= currentSubWave + 1) {
                 Debug.Log("All subwaves cleared!");
@@ -34,27 +49,34 @@ namespace Waves {
             return true;
         }
 
+        /// <summary>
+        ///     Spawns the enemies in the current sub wave.
+        /// </summary>
         public void SpawnSubWave() {
-            Debug.Log("Spawning sub wave " + currentSubWave);
+            //Debug.Log("Spawning sub wave " + currentSubWave);
             foreach (EnemyListEntry enemySet in waveManagerSO.waves[currentWave].subWaves[currentSubWave].setEnemyTypes) {
                 
-                Debug.Log("Spawning " + enemySet.count + " " + enemySet.enemy + " enemies!"); 
+                //Debug.Log("Spawning " + enemySet.count + " " + enemySet.enemy + " enemies!"); 
                 
                 for (int i = 0; i < enemySet.count; i++) {
                     GetValidSpawnLocation();
-                    //Instantiate enemy
+                    //TODO: Instantiate the enemy.
                 }
                 
             }
         }
 
+        /// <summary>
+        ///     Gets a random valid spawn location for enemies.
+        /// </summary>
+        /// <returns>The spawn position.</returns>
         public Vector3 GetValidSpawnLocation() {
             return Vector3.zero; //TODO: Find a valid location, after rink impl.
         }
 
         public void Update() {
             
-            
+            // Debug
             if (Keyboard.current != null)
             {
                 if (Keyboard.current.spaceKey.wasPressedThisFrame) {
