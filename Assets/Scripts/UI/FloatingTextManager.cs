ec2ffd74dbe66a45b1eeebcc2d4f670b080c5bb7
EvanMcRae <evanmmcrae@gmail.com>
2025-08-02 13:14:56 -0400

    Get text to spawn beneath other stuff

--- Assets/Scripts/UI/FloatingTextManager.cs
+++ Assets/Scripts/UI/FloatingTextManager.cs
@@ -5,7 +5,6 @@ public class FloatingTextManager : MonoBehaviour
 {
     public GameObject floatingSpritePrefab;
     public static FloatingTextManager instance;
-    public Canvas canvas;
 
     public void Start()
     {
@@ -17,12 +16,10 @@ public class FloatingTextManager : MonoBehaviour
         Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
 
         // Make instance under canvas
-        GameObject instance = Instantiate(floatingSpritePrefab, canvas.transform);
+        GameObject instance = Instantiate(floatingSpritePrefab, transform);
 
         // Convert screen point to canvas local point
-        Debug.Log(canvas);
-
-        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+        RectTransform canvasRect = transform.parent.GetComponent<RectTransform>();
         RectTransform instanceRect = instance.GetComponent<RectTransform>();
 
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, screenPosition, null, out Vector2 anchoredPos))
