8bf4a878fe9fb4df422c7fe264ced71f809e1cc3
RJWMD <113476572+RJWMD@users.noreply.github.com>
2025-08-03 11:54:33 -0400

    Fixed enemies not playing idle animations, made idle animations loop

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -48,7 +48,7 @@ namespace Enemies {
             SetCanMove(false); //Starting value false
             EnemySpawned();
             //anim = gameObject.GetComponent<Animator>(); //Commented out so that this can be set in inspector, hopefully this doesnt break anything
-            m_waiting = false; //TODO: change to true when implemented with waves
+            //m_waiting = false; //TODO: change to true when implemented with waves
             rb = GetComponent<Rigidbody>();
             if (!rb)
             {
