1ace3bc45bfdb20ecf8c1b904f4fe9829bd88362
LupidCorvid <lucidcanine921@gmail.com>
2025-08-03 00:35:49 -0400

    Visual effect

--- Assets/Scripts/introCutscene.cs
+++ Assets/Scripts/introCutscene.cs
@@ -11,11 +11,17 @@ public class introCutscene : MonoBehaviour
     public TMP_Text text;
     public InputManager imso;
 
+    float frame1 = .05f;
+    float frame2 = 1f;
+    float timer = 0f;
+    float moveSpeed = 2f;
+
     public bool gameStarted = false;
-    string[] dialogue = { "Holy Bingle",
-                          "We got the no fly list",
-                          "We actually did it",
-                          "I'm gonna skate around in celebration."};
+    string[] dialogue = { "Sheesh, who let all of these creatures into the rink?!",
+                          "I cant skate around in peace with all this commotion.",
+                          "Hmmm... I know!",
+                          "Ill skate in circles to cut holes in the ice.",
+                          "Then the creatures will fall right in!"};
     int index = 0;
 
     public void Start()
@@ -26,23 +32,42 @@ public class introCutscene : MonoBehaviour
     public void Update()
     {
         if (PauseManager.ShouldNotRun()) return;
-
-        if (imso.jump.action.WasPressedThisFrame() && !gameStarted)
-        {
-            index += 1;
-            if (index >= dialogue.Length) gameStarted = true;
-            else
-            {
-                text.text = dialogue[index];
-            }
-        }
-
+        
         if (gameStarted)
         {
-            player.GetComponent<PlayerController>().startCutsceneActive = false;
-            gameManager.GetComponent<GameController>().StartController();
-            //Instantiate(gameManager);
-            Destroy(gameObject);
+            if (timer < frame1)
+            {
+                charSprite.transform.position = new Vector3(charSprite.transform.position.x, charSprite.transform.position.y + moveSpeed, charSprite.transform.position.z);
+                textBox.transform.position = new Vector3(textBox.transform.position.x, textBox.transform.position.y + moveSpeed, textBox.transform.position.z);
+            }
+            else if (frame1 <= timer && timer < frame2)
+            {
+                charSprite.transform.position = new Vector3(charSprite.transform.position.x, charSprite.transform.position.y - moveSpeed, charSprite.transform.position.z);
+                textBox.transform.position = new Vector3(textBox.transform.position.x, textBox.transform.position.y - moveSpeed, textBox.transform.position.z);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            if (imso.jump.action.WasPressedThisFrame() && !gameStarted)
+            {
+                index += 1;
+                if (index >= dialogue.Length)
+                {
+                    gameStarted = true;
+                    text.text = "";
+                    player.GetComponent<PlayerController>().startCutsceneActive = false;
+                    gameManager.GetComponent<GameController>().StartController();
+                }
+                else
+                {
+                    text.text = dialogue[index];
+                }
+            }
         }
     }
 }
